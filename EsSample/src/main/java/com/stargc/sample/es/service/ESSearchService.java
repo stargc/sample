3ef77a09eacc738456e918633cedcd40c500f685
guanchen <roseraining2@126.com>
2020-05-08 18:18:10 +0800

    es 全文检索2

--- EsSample/src/main/java/com/stargc/sample/es/service/ESSearchService.java
+++ EsSample/src/main/java/com/stargc/sample/es/service/ESSearchService.java
@@ -1,16 +1,16 @@
 package com.stargc.sample.es.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.util.CollectionUtils;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.MultiMatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.ScoreSortBuilder;
@@ -34,48 +34,19 @@ public class ESSearchService {
     @Resource
     RestHighLevelClient restHighLevelClient;
 
-    public List<Map<String, Object>> search() {
-        List<Map<String, Object>> result = Collections.EMPTY_LIST;
-        try {
-            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-            SearchRequest request = new SearchRequest();
-            //索引
-            request.indices("systemlog");
-            sourceBuilder.from(0);
-            sourceBuilder.size();
-
-            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
-            boolQueryBuilder.must(QueryBuilders.wildcardQuery("bussiness","test1"));
-            sourceBuilder.query(boolQueryBuilder);
-
-            sourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
-            sourceBuilder.fetchSource(new String[]{"bussiness","operationDescription"},null);
-
-            //各种组合条件
-            request.source(sourceBuilder);
-            //请求
-            log.info(String.format("查询条件：%s",request.source().toString()));
-            SearchResponse response = restHighLevelClient.search(request);
-            //解析返回
-            if (response.status() != RestStatus.OK || response.getHits().getTotalHits() <= 0) {
-                result = Collections.emptyList();
-            }
-            //获取source
-            result = Arrays.stream(response.getHits().getHits()).map(b -> b.getSourceAsMap()).collect(Collectors.toList());
-        } catch (IOException e) {
-            log.error(ExceptionUtils.getStackTrace(e));
-        }
-        return result;
-    }
-
-
     public List<Map<String, Object>> multiSearch(String searchText) {
-        List<Map<String, Object>> result = Collections.EMPTY_LIST;
+        if (StringUtils.isBlank(searchText)) {
+            searchText = "*";
+        }
+        searchText = searchText.replace("%", "*");
+
+        List<String> texts = Arrays.stream(searchText.split(" ")).collect(Collectors.toList());
+        List<Map<String, Object>> result = Collections.emptyList();
         try {
             SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
             SearchRequest request = new SearchRequest();
             //索引
-            request.indices("systemlog");
+            request.indices("test22");
             //分页
             sourceBuilder.from(0);
             sourceBuilder.size(10);
@@ -83,10 +54,17 @@ public class ESSearchService {
 //            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
 //            boolQueryBuilder.must(QueryBuilders.wildcardQuery("bussiness","test1"));
 //            sourceBuilder.query(boolQueryBuilder);
+//            MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery(searchText);
+//            QueryStringQueryBuilder qb = QueryBuilders.queryStringQuery(searchText);
 
             //全文检索
-            MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery(searchText);
-            sourceBuilder.query(multiMatchQueryBuilder);
+            BoolQueryBuilder query = QueryBuilders.boolQuery();
+            texts.forEach(text -> {
+                query.must(QueryBuilders.boolQuery()
+                        .filter(QueryBuilders.wildcardQuery("all_text", text)));
+            });
+
+            sourceBuilder.query(query);
 
             //按评分排序
             sourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
@@ -94,15 +72,15 @@ public class ESSearchService {
             //各种组合条件
             request.source(sourceBuilder);
             //请求
-            log.info(String.format("查询条件：%s",request.source().toString()));
+            log.info(String.format("查询条件：%s", request.source().toString()));
             SearchResponse response = restHighLevelClient.search(request);
             //解析返回
             if (response.status() != RestStatus.OK || response.getHits().getTotalHits() <= 0) {
-                result = Collections.emptyList();
+                return result;
             }
-            log.info("获取结果总数：{}",response.getHits().getTotalHits() );
+            log.info("获取结果总数：{}", response.getHits().getTotalHits());
             //获取source
-            result = Arrays.stream(response.getHits().getHits()).map(b -> b.getSourceAsMap()).collect(Collectors.toList());
+            result = Arrays.stream(response.getHits().getHits()).map(SearchHit::getSourceAsMap).collect(Collectors.toList());
         } catch (IOException e) {
             log.error(ExceptionUtils.getStackTrace(e));
         }
@@ -178,7 +156,7 @@ public class ESSearchService {
             request.source(sourceBuilder);
 
             //请求
-            log.info(String.format("查询条件：%s",request.source().toString()));
+            log.info(String.format("查询条件：%s", request.source().toString()));
             SearchResponse rp = restHighLevelClient.search(request);
 
             //解析返回
